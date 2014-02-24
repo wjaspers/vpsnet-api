a1ae4828db4b3f5a69ff771da8a418059033811e
Will <wjaspers4@gmail.com>
2014-02-23 23:57:07 -0600

    Fix formatting.

--- lib/VPSNet/VirtualMachine/Backup/Backup.php
+++ lib/VPSNet/VirtualMachine/Backup/Backup.php
@@ -2,23 +2,20 @@
 
 namespace VPSNet\VirtualMachine\Backup;
 
-/**
- * Backups class
- */
 class Backup
 {
-    /**
-     * @var int
-     */
-    protected $id;
-
-
-    /**
-     * @var string
-     */
-    protected $label;
-
-
+	/**
+	 * @var int
+	 */
+	protected $id;
+	
+	
+	/**
+	 * @var string
+	 */
+	protected $label;
+	
+	
 	/**
 	 * @param string $label
 	 */
@@ -26,52 +23,52 @@ class Backup
 	{
 		$this->label = $label;
 	}
-
-
-    /**
-     * Returns the backup id.
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-
-    /**
-     * Sets the backup id.
-     *
-     * @param int $id
-     * @return Backup
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-        return $this;
-    }
-
-
-    /**
-     * Returns the label for this backup.
-     *
-     * @return string
-     */
-    public function getLabel()
-    {
-        return $this->label;
-    }
-
-
-    /**
-     * Sets the label.
-     *
-     * @param string $label
-     * @return Backup
-     */
-    public function setLabel($label)
-    {
-        $this->label = $label;
-        return $this;
-    }
+	
+	
+	/**
+	 * Returns the backup id.
+	 *
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	
+	/**
+	 * Sets the backup id.
+	 *
+	 * @param int $id
+	 * @return Backup
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+		return $this;
+	}
+	
+	
+	/**
+	 * Returns the label for this backup.
+	 *
+	 * @return string
+	 */
+	public function getLabel()
+	{
+		return $this->label;
+	}
+	
+	
+	/**
+	 * Sets the label.
+	 *
+	 * @param string $label
+	 * @return Backup
+	 */
+	public function setLabel($label)
+	{
+		$this->label = $label;
+		return $this;
+	}
 }
