d19755b84ffd89ae39495a2ea2702ca594828d42
Will <wjaspers4@gmail.com>
2014-04-01 22:43:25 -0500

    Remove setter
    After thinking about this for quite awhile, interfaces in PHP are better suited for getX than setX. Moreover, they are easier to work with because the setter's implementation need not be identical from one object to the next.

--- lib/VPSNet/Cloud/CloudAwareInterface.php
+++ lib/VPSNet/Cloud/CloudAwareInterface.php
@@ -5,13 +5,6 @@ namespace VPSNet\Cloud;
 interface CloudAwareInterface
 {
 	/**
-	 * @param Cloud $cloud
-	 *
-	 * @return CloudAware
-	 */
-	public function setCloud(Cloud $cloud);
-
-	/**
 	 * @return Cloud
 	 */
 	public function getCloud();
