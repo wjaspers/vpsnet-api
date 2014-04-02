dc0d374390b4801d5929037d537b5d819d767c44
Will <wjaspers4@gmail.com>
2014-04-01 22:47:34 -0500

    Add missing docblocks

--- lib/VPSNet/Node/StorageNode.php
+++ lib/VPSNet/Node/StorageNode.php
@@ -4,8 +4,15 @@ namespace VPSNet\Node;
 
 class StorageNode extends Node
 {
+	/**
+	 * @const string
+	 */
 	const TYPE = 'storage';
 
+
+	/**
+	 * @return string
+	 */
 	public function getType()
 	{
 		return self::TYPE;
