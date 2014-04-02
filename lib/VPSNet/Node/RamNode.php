cd932c3b92a614c500d61742a1cbd01cbdaabbaa
Will <wjaspers4@gmail.com>
2014-04-01 22:47:06 -0500

    Add missing docblocks

--- lib/VPSNet/Node/RamNode.php
+++ lib/VPSNet/Node/RamNode.php
@@ -4,8 +4,14 @@ namespace VPSNet\Node;
 
 class RamNode extends Node
 {
+	/**
+	 * @const string
+	 */
 	const TYPE = 'ram';
 
+	/**
+	 * @return string
+	 */
 	public function getType()
 	{
 		return self::TYPE;
